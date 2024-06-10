33dc09f235980e158b5ab6a49b7fffcb11b2ade2
Lorenzo Filippini <104449879+Desk888@users.noreply.github.com>
2024-06-10 12:28:56 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,124 @@
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Configurations
+let config = {
+    DATE_RANGE: last_n_days(30),
+    SPREADSHEET_URL: "", // Only include URL until /edit.
+    EMAIL_ADDRESSES: "" // Add email addresses
+  };
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function main() {
+
+    // Add script functionality here
+
+}
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// ADDITIONAL FUNCTIONALITIES BELOW
+
+// Custom Date Range Functionality
+function last_n_days(n) {
+    var from = new Date();
+    var to = new Date();
+    to.setDate(to.getDate() - n);
+    from.setDate(from.getDate() - 1);
+  
+    return google_date_range(to, from);
+  } // function last_n_days()
+  
+
+  function google_date_range(from, to) {
+  
+    function google_format(date) {
+      var date_array = [
+        date.getUTCFullYear(),
+        (date.getUTCMonth() + 1).toString().padStart(2, '0'),
+        date.getUTCDate().toString().padStart(2, '0')
+      ];
+      return date_array.join('');
+    }
+  
+    var inverse = (from > to);
+    from = google_format(from);
+    to = google_format(to);
+    var result = [from, to];
+  
+    if (inverse) {
+      result = [to, from];
+    }
+  
+    return result.join(',');
+  } // function google_date_range()
+
+
+// Sheet Creation
+function checkTab(file) {
+    var spreadsheet = SpreadsheetApp.openById(file.getId());
+    var currentDate = new Date();
+    var sheetName = currentDate.toISOString().slice(0, 10);
+  
+    var tab = spreadsheet.getSheetByName(sheetName);
+    if (tab) {
+      if (config.LOG === true) {
+        Logger.log("Selected tab " + sheetName);
+      }
+    } else {
+      tab = spreadsheet.insertSheet(sheetName);
+      if (config.LOG === true) {
+        Logger.log("Created tab " + sheetName);
+      }
+    }
+  
+    var defaultSheetDutch = spreadsheet.getSheetByName("Blad1");
+    if (defaultSheetDutch) {
+      spreadsheet.deleteSheet(defaultSheetDutch);
+    }
+  
+    var defaultSheetEnglish = spreadsheet.getSheetByName("Sheet1");
+    if (defaultSheetEnglish) {
+      spreadsheet.deleteSheet(defaultSheetEnglish);
+    }
+  
+    return tab;
+  } // function checkTab
+
+
+ // Send Email Functionality
+ var recipientEmails = config.EMAIL_ADDRESSES.split(',');
+ var subject = "PMAX Search Terms Report [UK]";
+ var body =
+   "The PMAX Search Terms Report has been generated and is available at: " +
+   config.SPREADSHEET_URL +
+   "\n\nReport covers the last " +
+   config.DATE_RANGE +
+   " days." +
+   "\n\nThis is an automated email sent by Google Ads Script.";
+
+ MailApp.sendEmail(recipientEmails.join(','), subject, body);
+
+ // Spreadsheet Formatting
+function formatSheet(sheet) {
+    Logger.log("Starting formatSheet function");
+  
+    if (!sheet) {
+      Logger.log("Error: The sheet object is not valid.");
+      return;
+    }
+    Logger.log("Sheet name: " + sheet.getName());
+    Logger.log("Sheet rows: " + sheet.getLastRow() + ", columns: " + sheet.getLastColumn());
+  
+    var headerRange = sheet.getRange(1, 1, 1, sheet.getLastColumn());
+    Logger.log("Header Range: " + headerRange.getA1Notation());
+    headerRange.setFontWeight('bold');
+    headerRange.setBackground('#4caf50');
+    headerRange.setFontColor('white');
+    sheet.autoResizeColumns(1, sheet.getLastColumn());
+    Logger.log("Completed formatSheet function");
+}
+
+ ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
